7e9c2c0f3bc8d54d1e47a1d325fc4eb18656b5ea
Robertjsr <Eryero02@gmail.com>
2025-04-22 08:54:39 +0000

    To Do List

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [ inputValue, setInputValue ] = useState("");
+	const [ toDoList, setToDoList ] = useState([]);
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1>To Do List</h1>
+            <ul>
+				<li><input type="text"
+				onChange={(e) => setInputValue(e.target.value)}
+				value={inputValue}
+				onKeyPress={(e) => {
+					if (e.key === "Enter") {
+						setToDoList(toDoList.concat ([inputValue]));
+						setInputValue("");
+					}
+				}}
+				placeholder="Mis cosas por hacer"></input>
+				</li>
+				{toDoList.map((item, index) => (
+						<li>
+							{item}{" "}
+							<i 
+							className="fa-solid fa-trash-can"  
+							onClick={() =>
+								setToDoList(
+									 toDoList.filter(
+										(t, currentIndex) => 
+											index != currentIndex
+									)
+								)
+							}></i>
+						</li>
+					))
+				}
+			</ul>
+			<div>{toDoList.length} Tareas Pendientes</div>
 		</div>
 	);
 };
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -11,6 +11,9 @@ import '../styles/index.css'
 // components
 import Home from './components/Home';
 
+// import fontawesome
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Home/>
